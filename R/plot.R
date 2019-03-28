d3bd67722306727b17a840fbe1577c74d52fe753
Kidus Asfaw <kidusasfaw1990@gmail.com>
2019-03-28 14:52:24 -0400

    final touches on igirfd_spatpomp plotting

--- R/plot.R
+++ R/plot.R
@@ -23,12 +23,13 @@ setMethod(
   signature=signature(x="igirfd_spatpomp"),
   definition=function (x, ...) {
     plot.df <- data.frame(x@traces)
+    cn <- colnames(plot.df)
     plot.df <- cbind(c(seq_len(dim(plot.df)[1])), plot.df)
-    names(plot.df) <- c("iteration", names(plot.df))
+    names(plot.df) <- c("iteration", cn)
     to.gather <- colnames(plot.df)[2:length(colnames(plot.df))]
     to.plot <- plot.df %>% tidyr::gather_(key = "param", val = "value", to.gather)
-    ggplot(data = to.plot) +
-      geom_point(mapping = aes(x = iteration, y = value)) +
-      facet_wrap(~key, ncol = 2)
+    ggplot2::ggplot(data = to.plot) +
+      geom_line(mapping = aes(x = iteration, y = value)) +
+      facet_wrap(~param, ncol = 3, scales = "free")
   }
 )
